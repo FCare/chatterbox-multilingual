be846a95d2b284bc63b74ad0cfcc4e19e658e96f
FCare <francois.caron.perso@gmail.com>
2025-12-03 10:15:28 +0100

    Reintroduce alignment analysis

--- src/chatterbox/models/t3/t3.py
+++ src/chatterbox/models/t3/t3.py
@@ -245,23 +245,23 @@ class T3(nn.Module):
         # with self.compile_lock:
         if not self.compiled:
             # Default to None for English models, only create for multilingual
-            # alignment_stream_analyzer = None
-            # if self.hp.is_multilingual:
-            #     alignment_stream_analyzer = AlignmentStreamAnalyzer(
-            #         self.tfmr,
-            #         None,
-            #         text_tokens_slice=(len_cond, len_cond + text_tokens_size),
-            #         alignment_layer_idx=9, # TODO: hparam or something?
-            #         eos_idx=self.hp.stop_speech_token,
-            #     )
-            #     assert alignment_stream_analyzer.eos_idx == self.hp.stop_speech_token
+            alignment_stream_analyzer = None
+            if self.hp.is_multilingual:
+                alignment_stream_analyzer = AlignmentStreamAnalyzer(
+                    self.tfmr,
+                    None,
+                    text_tokens_slice=(len_cond, len_cond + text_tokens_size),
+                    alignment_layer_idx=9, # TODO: hparam or something?
+                    eos_idx=self.hp.stop_speech_token,
+                )
+                assert alignment_stream_analyzer.eos_idx == self.hp.stop_speech_token
 
             patched_model = T3HuggingfaceBackend(
                 config=self.cfg,
                 llama=self.tfmr,
                 speech_enc=self.speech_emb,
                 speech_head=self.speech_head,
-                alignment_stream_analyzer=None,
+                alignment_stream_analyzer=alignment_stream_analyzer,
             )
             self.patched_model = patched_model
             self.compiled = True
